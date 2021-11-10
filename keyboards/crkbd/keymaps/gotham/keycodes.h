8fdef0a023ea4d15c07051c9fd65896e740590e3
Leandro Queiroz <leandro.queiroz@zup.com.br>
2021-11-10 08:23:06 -0300

    last change

--- keyboards/crkbd/keymaps/gotham/keycodes.h
+++ keyboards/crkbd/keymaps/gotham/keycodes.h
@@ -9,28 +9,21 @@ enum layers {
 
 enum custom_keycodes { QWERTY = SAFE_RANGE, LOWER, RAISE, ADJUST, RGBRST };
 
-// #define SFT_EQ MT(MOD_LSFT, KC_EQL)
-// #define SFT_QT MT(MOD_RSFT, KC_QUOT)
-
 #define SFT_A MT(MOD_LSFT, KC_A)
 #define CTL_Z MT(MOD_LCTL, KC_Z)
 
 #define SFT_SCLN MT(MOD_RSFT, KC_SCLN)
 #define CTL_SLSH MT(MOD_RCTL, KC_SLSH)
 
-#define CTL_SPC CTL_T(KC_SPC)
-// #define LOW_ESC (_LOWER, KC_ESC)
+#define SFT_SPC SFT_T(KC_SPC)
 #define GUI_ESC GUI_T(KC_ESC)
-// #define RAI_EQ LT(_RAISE, KC_EQL)
+#define SFT_QUOT SFT_T(KC_QUOT)
 #define SFT_ENT SFT_T(KC_ENT)
 #define RAISE  MO(_RAISE)
 #define LOWER MO(_LOWER)
 #define LOW_BSP LT(_LOWER, KC_BSPC)
 #define LANG LCA(KC_SPC)
 #define CMD_SPC G(KC_SPC)
-// #define TALL RSA(KC_A)
-// #define WIDE RSA(KC_S)
-// #define FULL RSA(KC_D)
 #define NAV_TL G(S(KC_LBRC))
 #define NAV_TR G(S(KC_RBRC))
 #define NAV_SL C(KC_LEFT)
@@ -43,8 +36,7 @@ enum custom_keycodes { QWERTY = SAFE_RANGE, LOWER, RAISE, ADJUST, RGBRST };
 #define TER_ML C(G(KC_L))
 #define TER_MB C(G(KC_B))
 #define SCR_SHOT G(S(KC_4))
-// #define LOW_DEL LT(_LOWER, KC_DEL)
 #define CTL_LANG MT(MOD_RCTL, LANG)
-// #define ADJ_GRV LT(_ADJUST, KC_GRV)
-// #define KC_ANGL LSFT(KC_COMM)
-// #define KC_ANGR LSFT(KC_DOT)
+
+// TMUX SHORTCUTS
+#define TMXPREFIX C(KC_B)
--- keyboards/crkbd/keymaps/gotham/keymap.c
+++ keyboards/crkbd/keymaps/gotham/keymap.c
@@ -15,11 +15,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|-----------------------------------------------------|                    |-----------------------------------------------------|
      KC_TAB,      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,     KC_I,     KC_O,    KC_P,  KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-     KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,    KC_J,    KC_K,    KC_L,  KC_SCLN, KC_QUOT,
+     KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,    KC_J,    KC_K,    KC_L,  KC_SCLN,  SFT_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-     KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                       KC_N,    KC_M, KC_COMM,  KC_DOT,  KC_SLSH,  LANG,
+     KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                       KC_N,    KC_M, KC_COMM,  KC_DOT,  KC_SLSH,  KC_RCTL,
   //---------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          GUI_ESC, LOWER, CTL_SPC,      SFT_ENT, RAISE, KC_RALT
+                                          GUI_ESC, LOWER, SFT_SPC,      KC_ENT, RAISE, KC_RALT
                                       //|--------------------------|  |--------------------------|
   ),
 
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       CMD_SPC,   KC_NO,    KC_NO,  KC_NO,  NAV_TL,  NAV_TR,                      KC_LEFT, KC_DOWN,  KC_UP, KC_RIGHT,   KC_NO, _______,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        CMD_9,   KC_NO,   KC_NO,RALT(KC_C),  NAV_SL,  NAV_SR,                        KC_NO,   KC_NO,  KC_NO,   KC_NO,    KC_NO,   KC_NO, 
+        CMD_9,   KC_NO,   KC_NO,RALT(KC_C),  NAV_SL,  NAV_SR,                      KC_NO,   KC_NO,  KC_NO,   KC_NO,    KC_NO,    LANG, 
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                          _______, _______, _______,    _______, MO(_ADJUST), KC_LGUI
                                       //|--------------------------|  |-------------------------------|
